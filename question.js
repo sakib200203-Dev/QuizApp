f2e18f71fc1ce4e1ebafef1d6278c4a49ee2846c
Sakib206039-debugger <sakib206039@gmail.com>
2026-07-05 21:48:21 +0600

    added question part

---
+++ question.js
@@ -0,0 +1,52 @@
+const questions = [
+  {
+    question: "Which language is used to style web pages?",
+    answers: ["HTML", "CSS", "JavaScript", "Python"],
+    correct: 1
+  },
+  {
+    question: "Which HTML tag is used for the largest heading?",
+    answers: ["<h6>", "<head>", "<h1>", "<title>"],
+    correct: 2
+  },
+  {
+    question: "Which keyword is used to declare a variable in JavaScript?",
+    answers: ["var", "let", "const", "All of the above"],
+    correct: 3
+  },
+  {
+    question: "Which company developed JavaScript?",
+    answers: ["Microsoft", "Netscape", "Google", "Apple"],
+    correct: 1
+  },
+  {
+    question: "Which symbol is used for comments in JavaScript?",
+    answers: ["<!-- -->", "//", "**", "##"],
+    correct: 1
+  },
+  {
+    question: "Which CSS property changes text color?",
+    answers: ["font-color", "text-color", "color", "background"],
+    correct: 2
+  },
+  {
+    question: "Which HTML element is used to create a button?",
+    answers: ["<btn>", "<button>", "<input>", "<click>"],
+    correct: 1
+  },
+  {
+    question: "Which method is used to print something in the browser console?",
+    answers: ["console.log()", "print()", "echo()", "document.write()"],
+    correct: 0
+  },
+  {
+    question: "Which operator checks both value and type in JavaScript?",
+    answers: ["==", "=", "===", "!="],
+    correct: 2
+  },
+  {
+    question: "Which HTML tag is used to insert an image?",
+    answers: ["<picture>", "<img>", "<image>", "<src>"],
+    correct: 1
+  }
+];
